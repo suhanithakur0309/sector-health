bb79449cedf8dff5f51cbdc41851772c5dd271d4
Suhani Thakur <thakur.suhani2006@gmail.com>
2026-05-30 22:02:38 +0530

    Pipeline Complete

--- pipeline.py
+++ pipeline.py
@@ -10,18 +10,21 @@ print(df.isnull().sum())
 #Filling null values with the previous day's closing price
 df.ffill(inplace=True)
 #Calculating daily returns for each sector
+#(gain or loss in one day)
 DR=df.pct_change()*100
 daily_returns=pd.DataFrame(DR)
 #Calculating votality for each sector
+#(how wildly prices move and down)
 vortaility=daily_returns.std()
 vortaility.sort_values(ascending=False,inplace=True)
-#Calculating rolling returns for the last 30, 60 and 90 days
-rolling_30=daily_returns.rolling(30).sum()
-rolling_60=daily_returns.rolling(60).sum()
-rolling_90=daily_returns.rolling(90).sum()
+#Calculating rolling average for the last 30, 60 and 90 days
+#(avg price of the last N days)
+rolling_30=daily_returns.rolling(30).mean()
+rolling_60=daily_returns.rolling(60).mean()
+rolling_90=daily_returns.rolling(90).mean()
 #Calculating momentum of each sector
+#(whether a sector is accelerating or deaccelerating)
 momentum=rolling_30-rolling_90
 #Calculating Relative Strength
 #(performance of a sector compared to the overall market)
 relative_strength=rolling_30.sub(rolling_30['^NSEI'],axis=0)
-relative_strength.head()
