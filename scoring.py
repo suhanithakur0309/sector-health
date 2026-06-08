ca258d9da120b6f83827401e8d7c01c93418fc86
Suhani Thakur <thakur.suhani2006@gmail.com>
2026-06-08 22:30:50 +0530

    Scoring Model Complete

--- scoring.py
+++ scoring.py
@@ -6,11 +6,11 @@ def normalize(series):
   return Z
 norm_rolling_30=normalize(rolling_30)
 norm_rolling_90=normalize(rolling_90)
-norm_volatility=normalize(-volatility)
+norm_volatility=normalize(-Volatility)
 norm_momentum=normalize(momentum)
-norm_Relative_Strength=normalize(Relative_Strength)
+norm_Relative_Strength=normalize(relative_strength)
 #Defining a combined weighted health score according to importance
-health_score=(0.25*norm_rolling_30)+(0.20*norm_rolling_90)+(0.20*volatility)+(0.20*norm_momentum)+(0.15*norm_Relative_Strength)
+health_score=(0.25*norm_rolling_30)+(0.20*norm_rolling_90)+(0.20*Volatility)+(0.20*norm_momentum)+(0.15*norm_Relative_Strength)
 health_score=health_score.drop(columns=['^NSEI'])
 #Scaling the health for ease of comparision
 health_score_scaled=(health_score-health_score.min())/(health_score.max()-health_score.min())*100
